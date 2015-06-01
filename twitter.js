a9e8921cf3072977dc5ab2818653883ef6a0600d
David de Bie <daviddebie09@gmail.com>
2015-06-01 16:10:49 +0200

    Finished Twitter feed

--- twitter.js
+++ twitter.js
@@ -203,104 +203,49 @@
  *     is "en" (English).
  */
 
-// ##### Simple example 1 #####
-// A simple example to get my latest tweet and write to a HTML element with
-// id "example1". Also automatically hyperlinks URLS and user mentions and
-// hashtags.
+
+// Michel Foucault
 var config1 = {
-  "id": '605293418609295360',
-  "domId": 'example1',
-  "maxTweets": 1,
-  "enableLinks": true,
-  "showPermalinks": false
+  "id": '605339248355622913',
+  "domId": 'foucault',
+  "maxTweets": 6,
+  "enableLinks": true
 };
 twitterFetcher.fetch(config1);
 
-// ##### Simple example 2 #####
-// A simple example to get my latest 5 of my favourite tweets and write to a
-// HTML element with id "talk". Also automatically hyperlinks URLS and user
-// mentions and hashtags but does not display time of post. We also make the
-// request to Twitter specifiying we would like results where possible in
-// English language.
+// Martin Heidegger
 var config2 = {
-  "id": '347099293930377217',
-  "domId": 'example2',
-  "maxTweets": 5,
+  "id": '605341673351184384',
+  "domId": 'heidegger',
+  "maxTweets": 6,
   "enableLinks": true,
-  "showUser": true,
-  "showTime": true,
-  "lang": 'en'
-};
+  };
 twitterFetcher.fetch(config2);
 
-// ##### Simple example 3 #####
-// A simple example to get latest 5 tweets for #API tag and shows any images
-// attached to tweets.
+// Sigmund Freud
 var config3 = {
-  "id": '502160051226681344',
-  "domId": 'example3',
-  "maxTweets": 5,
+  "id": '605344415289327616',
+  "domId": 'freud',
+  "maxTweets": 6,
   "enableLinks": true,
-  "showImages": true
 };
 twitterFetcher.fetch(config3);
 
-// ##### Advanced example #####
-// An advance example to get latest 5 posts using hashtag #API and write to a
-// HTML element with id "tweets2" without showing user details and using a
-// custom format to display the date/time of the post, and does not show
-// retweets.
+// Walter Benjamin
 var config4 = {
-  "id": '345690956013633536',
-  "domId": 'example4',
-  "maxTweets": 3,
+  "id": '605355916762984448',
+  "domId": 'benjamin',
+  "maxTweets": 6,
   "enableLinks": true,
-  "showUser": false,
-  "showTime": true,
-  "dateFunction": dateFormatter,
-  "showRetweet": false
 };
-
-// For advanced example which allows you to customize how tweet time is
-// formatted you simply define a function which takes a JavaScript date as a
-// parameter and returns a string!
-// See http://www.w3schools.com/jsref/jsref_obj_date.asp for properties
-// of a Date object.
-function dateFormatter(date) {
-  return date.toTimeString();
-}
-
 twitterFetcher.fetch(config4);
 
-// ##### Advanced example 2 #####
-// Similar as previous, except this time we pass a custom function to render the
-// tweets ourself! Useful if you need to know exactly when data has returned or
-// if you need full control over the output.
 
+// Immanuel Kant
 var config5 = {
-  "id": '345690956013633536',
-  "domId": '',
-  "maxTweets": 3,
+  "id": '605363416841252866',
+  "domId": 'kant',
+  "maxTweets": 6,
   "enableLinks": true,
-  "showUser": true,
-  "showTime": true,
-  "dateFunction": '',
-  "showRetweet": false,
-  "customCallback": handleTweets,
-  "showInteraction": false
 };
-
-function handleTweets(tweets) {
-  var x = tweets.length;
-  var n = 0;
-  var element = document.getElementById('example5');
-  var html = '<ul>';
-  while (n < x) {
-    html += '<li>' + tweets[n] + '</li>';
-    n++;
-  }
-  html += '</ul>';
-  element.innerHTML = html;
-}
-
 twitterFetcher.fetch(config5);
